4d0519d42cda38962686b1712574270f1343b1c6
dingzu <dzwangyihan@gmail.com>
2022-05-07 11:03:58 +0800

    优化

--- src/code.ts
+++ src/code.ts
@@ -1,7 +1,5 @@
-import { dispatch, handleEvent } from './utils/codeMessageHandler';
 import appLoader from './utils/appLoader'
 import start from './utils/start'
-figma.showUI(__html__);
 
 // 安装 app
 appLoader.load()
--- src/utils/appLoader.ts
+++ src/utils/appLoader.ts
@@ -1,6 +1,6 @@
 // 自动引入全部依赖
 // @ts-ignore
-const modulesFiles = require.context('./app', true, /.ts$/)
+const modulesFiles = require.context('./app', true, /\.ts$/)
 const modules = modulesFiles.keys().reduce((module, modulePath) => {
     const moduleName = modulePath.replace(/^.\/(.*)\.js/, '$1')
     const value = modulesFiles(modulePath)
