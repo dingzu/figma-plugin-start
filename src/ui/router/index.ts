1cdebc53243fe911173f0be354ea564a06760bc4
dingzu <dzwangyihan@gmail.com>
2022-05-07 16:41:21 +0800

    识别组件

--- src/ui/router/index.ts
+++ src/ui/router/index.ts
@@ -1,9 +1,18 @@
 import { createRouter, createWebHashHistory } from "vue-router"
+const home = () => import('../views/Home.vue')
+const login = () => import('../views/Home.vue')
 
 const routes = [
+  { path: "/", name: "mian", redirect: "/home" },
   {
-    path: '/',
-    component: () => import('../views/Home.vue')
+    path: "/home",
+    name: "home",
+    component: home
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: login
   }
 ]
 export const router = createRouter({
